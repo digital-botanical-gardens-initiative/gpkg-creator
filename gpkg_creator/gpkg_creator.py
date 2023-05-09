66123fd6746e1040f8676fe947e8a77862b45126
edouardbruelhart <edouard.bruelhart@unifr.ch>
2023-05-09 11:32:09 +0200

    up basic structure

--- gpkg_creator/gpkg_creator.py
+++ gpkg_creator/gpkg_creator.py
@@ -21,217 +21,66 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
-from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction, QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QDialogButtonBox
-from qgis.core import QgsProject, QgsVectorLayer, QgsFields, QgsField, QgsGeometry, QgsFeature, QgsCoordinateReferenceSystem
-from qgis.utils import iface
+from qgis.PyQt.QtGui import *
+from qgis.PyQt.QtWidgets import *
+from qgis.core import QgsVectorLayer, QgsVectorFileWriter, QgsField, QgsProject
+from qgis.PyQt.QtCore import QVariant
+import os
 
-# Initialize Qt resources from file resources.py
-from .resources import *
-# Import the code for the dialog
-from .gpkg_creator_dialog import GpkgCreatorDialog
-import os.path
+# initialize Qt resources from file resources.py
+from . import resources
 
+def classFactory(iface):
+    return GpkgCreator(iface)
 
-class GpkgCreator(QDialog):
-    """QGIS Plugin Implementation."""
-
+class GpkgCreator:
     def __init__(self, iface):
-        """Constructor.
-
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgsInterface
-        """
-        # Save reference to the QGIS interface
         self.iface = iface
-        # initialize plugin directory
-        self.plugin_dir = os.path.dirname(__file__)
-        # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'GpkgCreator_{}.qm'.format(locale))
-
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-            QCoreApplication.installTranslator(self.translator)
-
-        # Declare instance attributes
-        self.actions = []
-        self.menu = self.tr(u'&DBGI Geopackage Creator')
-
-        # Check if plugin was started the first time in current QGIS session
-        # Must be set in initGui() to survive plugin reloads
-        self.first_start = None
-        QDialog.__init__(self)
-        self.setWindowTitle("GPKG Creator")
-
-        # Add the name input field
-        nameLabel = QLabel("Enter the name of the GPKG layer:")
-        self.nameLineEdit = QLineEdit()
-        self.nameLineEdit.setText("my_layer")
-        nameLayout = QVBoxLayout()
-        nameLayout.addWidget(nameLabel)
-        nameLayout.addWidget(self.nameLineEdit)
-
-        # Add the OK and Cancel buttons
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        buttonBox.accepted.connect(self.accept)
-        buttonBox.rejected.connect(self.reject)
-
-        # Combine the layouts and add to the dialog
-        layout = QVBoxLayout()
-        layout.addLayout(nameLayout)
-        layout.addWidget(buttonBox)
-        self.setLayout(layout)
-
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('GpkgCreator', message)
-
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            # Adds plugin icon to Plugins toolbar
-            self.iface.addToolBarIcon(action)
-
-        if add_to_menu:
-            self.iface.addPluginToVectorMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
 
     def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
+        # create action that will start plugin configuration
+        self.action = QAction(QIcon("resources:icon.png"),
+                          "Create a DBGI geopackage",
+                          self.iface.mainWindow())
+        self.action.setObjectName("testAction")
+        self.action.setWhatsThis("Configuration for test plugin")
+        self.action.setStatusTip("This is status tip")
+        self.action.triggered.connect(self.run)
 
-        icon_path = ':/plugins/gpkg_creator/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'Create a DBGI geopackage'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
-
-        # will be set False in run()
-        self.first_start = True
-
+        # add toolbar button and menu item
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToVectorMenu("&DBGI Geopackage Creator", self.action)
 
     def unload(self):
-        """Removes the plugin menu item and icon from QGIS GUI."""
-        for action in self.actions:
-            self.iface.removePluginVectorMenu(
-                self.tr(u'&DBGI Geopackage Creator'),
-                action)
-            self.iface.removeToolBarIcon(action)
-
+        # remove the plugin menu item and icon
+        self.iface.removePluginVectorMenu("&DBGI Geopackage Creator", self.action)
+        self.iface.removeToolBarIcon(self.action)
 
     def run(self):
-        """Run method that performs all the real work"""
+    # ask user to enter the name of the GPKG
+        gpkg_name, ok = QInputDialog.getText(self.iface.mainWindow(), "GPKG name", "Enter the name of the GPKG:")
+        if not ok or gpkg_name == "":
+            return
 
-        # Create the dialog with elements (after translation) and keep reference
-        # Only create GUI ONCE in callback, so that it will only load when the plugin is started
-        if self.first_start == True:
-            self.first_start = False
-            self.dlg = GpkgCreatorDialog()
+        # create the GPKG
+        layer_name = gpkg_name
+        layer_fields = [QgsField("Plant_ID", QVariant.String),
+                        QgsField("spl_code", QVariant.String),
+                        QgsField("Panel", QVariant.String),
+                        QgsField("General", QVariant.String),
+                        QgsField("Detail", QVariant.String),
+                        QgsField("Cut", QVariant.String),
+                        QgsField("Panel+Label", QVariant.String),
+                        QgsField("x_coord", QVariant.Double),
+                        QgsField("y_coord", QVariant.Double)]
+        crs = self.iface.mapCanvas().mapSettings().destinationCrs().authid()
+        gpkg_path = os.path.join(QgsProject.instance().homePath(), f"{gpkg_name}.gpkg")
+        gpkg_layer = QgsVectorLayer(f"Point?crs={crs}&field=name:string(50)&field=value:double", layer_name, "ogr")
+        gpkg_layer_provider = gpkg_layer.dataProvider()
 
-        # show the dialog
-        self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+        # add the fields to the layer
+        gpkg_layer_provider.addAttributes(layer_fields)
+        gpkg_layer.updateFields()
 
-    def accept(self):
-        # Create the layer
-        gpkg_file = QgsProject.instance().homePath() + "/my_gpkg.gpkg"
-        crs = iface.mapCanvas().mapSettings().destinationCrs().authid()
-        fields = QgsFields()
-        fields.append(QgsField("Name", QVariant.String))
-        writer = QgsVectorLayer(gpkg_file, self.nameLineEdit.text(), "ogr", fields, QgsWkbTypes.Point, crs)
-        QgsProject.instance().addMapLayer(writer)
-        self.close()
-
-def run(self):
-    dialog = GpkgCreatorg()
-    result = dialog.exec_()
+        # add the layer to the project
+        QgsProject.instance().addMapLayer(gpkg_layer)
